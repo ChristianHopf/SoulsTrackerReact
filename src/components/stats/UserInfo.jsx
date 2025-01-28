399565d65e36e2b3c18eccf7f3e3ca99df65c3dc
christian-hopf <christian-hopf@cognizant.com>
2025-01-28 12:48:48 -0500

    UserInfo gets user data from store

--- src/components/stats/UserInfo.jsx
+++ src/components/stats/UserInfo.jsx
@@ -1,30 +1,35 @@
 // import React from "react";
+import { useSelector } from "react-redux";
 
 function UserInfo() {
-  return (
-    // {{#if @user}}
-    <div data-test-user-info className="flex flex-row gap-4 mx-3 md:mx-0">
-      <img
-        src={"{@user.avatarmedium}"}
-        alt="avatar"
-        width="96"
-        className="border-2 border-accent-dark rounded-md"
-      />
-      <div className="flex flex-col items-start justify-between">
-        <h1 className="text-4xl md:text-5xl text-primary-dark">
-          @user.personaname
-        </h1>
-        <a
-          href="{{concat 'https://steamcommunity.com/profiles/' @user.steamid}}"
-          className="text-xl ml-1.5 text-accent-dark"
-          target="_blank"
-          rel="external nofollow noopener noreferrer"
-        >
-          View profile
-        </a>
+  const user = useSelector((state) => state.user.value);
+  console.log(user);
+
+  if (user && Object.keys(user).length > 0) {
+    return (
+      <div data-test-user-info className="flex flex-row gap-4 mx-3 md:mx-0">
+        <img
+          src={user.avatarmedium}
+          alt="avatar"
+          width="96"
+          className="border-2 border-accent-dark rounded-md"
+        />
+        <div className="flex flex-col items-start justify-between">
+          <h1 className="text-4xl md:text-5xl text-primary-dark">
+            {user.personaname}
+          </h1>
+          <a
+            href={`https://steamcommunity.com/profiles/${user.steamid}`}
+            className="text-xl ml-1.5 text-accent-dark"
+            target="_blank"
+            rel="external nofollow noopener noreferrer"
+          >
+            View profile
+          </a>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default UserInfo;
