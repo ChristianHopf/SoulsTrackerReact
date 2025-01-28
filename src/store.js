09d375c121865ccf38ac4cdcbfbeeb53ddc79bce
christian-hopf <christian-hopf@cognizant.com>
2025-01-28 13:14:52 -0500

    Add gamesReducer

--- src/store.js
+++ src/store.js
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./features/stats/userSlice";
+import gamesReducer from "./features/stats/gamesSlice";
+
+let rootReducer = combineReducers({
+  user: userReducer,
+  games: gamesReducer,
+});
 
 export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: rootReducer,
+  //   reducer: {
+  //     user: userReducer,
+  //   },
 });
