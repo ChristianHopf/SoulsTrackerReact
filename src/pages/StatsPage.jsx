41eb653445140e3e52a33ac479ed6e9f3c3e4703
christian-hopf <christian-hopf@cognizant.com>
2025-01-28 12:49:04 -0500

    Add UserInfo to StatsPage

--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -1,6 +1,7 @@
 // import React from 'react'
 import Header from "../components/layout/Header";
 import InputRow from "../components/input/InputRow";
+import UserInfo from "../components/stats/UserInfo";
 
 function StatsPage() {
   return (
@@ -8,6 +9,9 @@ function StatsPage() {
       <Header />
       <div className="container m-auto">
         <InputRow />
+        <div className="flex flex-col gap-4 max-w-6xl mx-auto py-4">
+          <UserInfo />
+        </div>
       </div>
     </div>
   );
