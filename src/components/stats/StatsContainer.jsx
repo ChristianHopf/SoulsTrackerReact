e9d74e3f0119c0cb4514eee67498ba008fd0d53f
christian-hopf <christian-hopf@cognizant.com>
2025-01-29 14:13:44 -0500

    StatsContainer subscribes to stats, loading, error

--- src/components/stats/StatsContainer.jsx
+++ src/components/stats/StatsContainer.jsx
@@ -2,17 +2,33 @@ import React from "react";
 import Playtime from "./Playtime";
 import Bosses from "./Bosses";
 import Achievements from "./Achievements";
+import { useSelector } from "react-redux";
+import Unsupported from "./Unsupported";
+// import Unsupported from "./Unsupported";
 
 function StatsContainer() {
-  return (
-    <div className="flex flex-col lg:flex-row gap-4">
-      <div className="flex flex-col w-full gap-4">
-        <Playtime />
-        <Bosses />
+  const { data, loading, error } = useSelector((state) => state.stats);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center">
+        <h1 className="text-5xl text-accent-dark">Loading...</h1>
       </div>
-      <Achievements />
-    </div>
-  );
+    );
+  } else if (error) {
+    return <Unsupported />;
+  } else if (data) {
+    // console.log(data);
+    return (
+      <div className="flex flex-col lg:flex-row gap-4">
+        <div className="flex flex-col w-full gap-4">
+          <Playtime />
+          <Bosses />
+        </div>
+        <Achievements />
+      </div>
+    );
+  }
 }
 
 export default StatsContainer;
