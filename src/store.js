104119f141f4ab977be9cbbcf34fbeba8e7e3598
christian-hopf <christian-hopf@cognizant.com>
2025-01-29 12:08:14 -0500

    Stats reducers

--- src/store.js
+++ src/store.js
@@ -2,10 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./features/stats/userSlice";
 import gamesReducer from "./features/stats/gamesSlice";
+import playtimeReducer from "./features/stats/playtimeSlice";
+import bossesReducer from "./features/stats/bossesSlice";
+import achievementsReducer from "./features/stats/achievementsSlice";
 
 let rootReducer = combineReducers({
   user: userReducer,
   games: gamesReducer,
+  playtime: playtimeReducer,
+  bosses: bossesReducer,
+  achievements: achievementsReducer,
 });
 
 export default configureStore({
